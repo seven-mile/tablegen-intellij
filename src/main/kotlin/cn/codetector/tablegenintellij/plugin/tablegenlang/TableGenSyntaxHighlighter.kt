34c6cafd0035a17ff7a477243d0a7c1b649540a8
Qihan Cai <qcai8733@uni.sydney.edu.au>
2023-04-28 04:33:25 +1000

    add syntax highlight for new statements, change version to git hash

--- src/main/kotlin/cn/codetector/tablegenintellij/plugin/tablegenlang/TableGenSyntaxHighlighter.kt
+++ src/main/kotlin/cn/codetector/tablegenintellij/plugin/tablegenlang/TableGenSyntaxHighlighter.kt
@@ -14,6 +14,7 @@ class TableGenSyntaxHighlighter : SyntaxHighlighterBase() {
     private val tokenHighlighters: MutableMap<IElementType, Array<TextAttributesKey>> = HashMap()
 
     init {
+        // Keywords
         tokenHighlighters[TableGenTypes.KEYWORDDEF] =
             arrayOf(createTextAttributesKey("TABLEGEN_DEF", DefaultLanguageHighlighterColors.KEYWORD))
         tokenHighlighters[TableGenTypes.KEYWORDLET] =
@@ -26,6 +27,8 @@ class TableGenSyntaxHighlighter : SyntaxHighlighterBase() {
             arrayOf(createTextAttributesKey("TABLEGEN_DAG", DefaultLanguageHighlighterColors.KEYWORD))
         tokenHighlighters[TableGenTypes.KEYWORDFIELD] =
             arrayOf(createTextAttributesKey("TABLEGEN_FIELD", DefaultLanguageHighlighterColors.KEYWORD))
+        tokenHighlighters[TableGenTypes.KEYWORDINCLUDE] =
+            arrayOf(createTextAttributesKey("TABLEGEN_INCLUDE", DefaultLanguageHighlighterColors.METADATA))
         tokenHighlighters[TableGenTypes.KEYWORDDEFM] =
             arrayOf(createTextAttributesKey("TABLEGEN_DEFM", DefaultLanguageHighlighterColors.KEYWORD))
         tokenHighlighters[TableGenTypes.KEYWORDFOREACH] =
@@ -40,9 +43,9 @@ class TableGenSyntaxHighlighter : SyntaxHighlighterBase() {
             arrayOf(createTextAttributesKey("TABLEGEN_THEN", DefaultLanguageHighlighterColors.KEYWORD))
         tokenHighlighters[TableGenTypes.KEYWORDELSE] =
             arrayOf(createTextAttributesKey("TABLEGEN_ELSE", DefaultLanguageHighlighterColors.KEYWORD))
+
         tokenHighlighters[TableGenTypes.KEYWORDCLASS] =
             arrayOf(createTextAttributesKey("TABLEGEN_CLASS", DefaultLanguageHighlighterColors.KEYWORD))
-
         tokenHighlighters[TableGenTypes.KEYWORDINT] =
             arrayOf(createTextAttributesKey("TABLEGEN_INT", DefaultLanguageHighlighterColors.KEYWORD))
         tokenHighlighters[TableGenTypes.KEYWORDSTRING] =
@@ -54,7 +57,7 @@ class TableGenSyntaxHighlighter : SyntaxHighlighterBase() {
         tokenHighlighters[TableGenTypes.KEYWORDBITS] =
             arrayOf(createTextAttributesKey("TABLEGEN_BITS", DefaultLanguageHighlighterColors.KEYWORD))
         tokenHighlighters[TableGenTypes.KEYWORDMULTICLASS] =
-                arrayOf(createTextAttributesKey("TABLEGEN_MULTICLASS", DefaultLanguageHighlighterColors.KEYWORD))
+            arrayOf(createTextAttributesKey("TABLEGEN_MULTICLASS", DefaultLanguageHighlighterColors.KEYWORD))
 
         // INCLUDE
         tokenHighlighters[TableGenTypes.KEYWORDINCLUDE] =
@@ -65,12 +68,112 @@ class TableGenSyntaxHighlighter : SyntaxHighlighterBase() {
             arrayOf(createTextAttributesKey("TABLEGEN_INTLIT", DefaultLanguageHighlighterColors.NUMBER))
         tokenHighlighters[TableGenTypes.STRING] =
             arrayOf(createTextAttributesKey("TABLEGEN_STRINGLIT", DefaultLanguageHighlighterColors.STRING))
+        tokenHighlighters[TableGenTypes.CODE_FRAGMENT] =
+            arrayOf(createTextAttributesKey("TABLEGEN_CODE_FRAGMENT", DefaultLanguageHighlighterColors.STRING))
+
+        // Identifiers
+        tokenHighlighters[TableGenTypes.IDENTIFIER] =
+            arrayOf(createTextAttributesKey("TABLEGEN_IDENTIFIER", DefaultLanguageHighlighterColors.IDENTIFIER))
+        tokenHighlighters[TableGenTypes.VARNAME] =
+            arrayOf(createTextAttributesKey("TABLEGEN_VARNAME", DefaultLanguageHighlighterColors.IDENTIFIER))
 
         // Comments
         tokenHighlighters[TableGenTypes.BLOCK_COMMENT] =
             arrayOf(createTextAttributesKey("TABLEGEN_BLOCK_COMMENTS", DefaultLanguageHighlighterColors.BLOCK_COMMENT))
         tokenHighlighters[TableGenTypes.LINE_COMMENT] =
             arrayOf(createTextAttributesKey("TABLEGEN_LINE_COMMENTS", DefaultLanguageHighlighterColors.LINE_COMMENT))
+
+        // Bang Operators
+        tokenHighlighters[TableGenTypes.CONDITION] =
+            arrayOf(createTextAttributesKey("TABLEGEN_CONDITION", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGADD] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_ADD", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGAND] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_AND", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGCAST] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_CAST", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGCON] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_CON", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGDAG] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_DAG", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGDIV] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_DIV", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGEMPTY] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_EMPTY", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGEQ] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_EQ", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGEXISTS] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_EXISTS", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGFILTER] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_FILTER", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGFIND] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_FIND", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGFOLDL] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_FOLDL", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGFOREACH] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_FOREACH", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGGE] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_GE", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGGETDAGOP] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_GETDAGOP", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGGT] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_GT", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGHEAD] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_HEAD", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGIF] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_IF", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGINTERLEAVE] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_INTERLEAVE", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGISA] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_ISA", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGLE] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_LE", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGLISTCONCAT] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_LISTCONCAT", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGLISTREMOVE] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_LISTREMOVE", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGLISTSPLAT] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_LISTSPLAT", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGLOGTWO] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_LOGTWO", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGLT] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_LT", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGMUL] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_MUL", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGNE] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_NE", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGNOT] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_NOT", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGOR] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_OR", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGRANGE] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_RANGE", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGSETDAGOP] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_SETDAGOP", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGSHL] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_SHL", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGSIZE] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_SIZE", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGSRA] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_SRA", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGSRL] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_SRL", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGSTRCONCAT] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_STRCONCAT", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGSUB] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_SUB", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGSUBST] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_SUBST", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGSUBSTR] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_SUBSTR", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGTAIL] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_TAIL", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGTOLOWER] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_TOLOWER", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGTOUPPER] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_BANGTOUPPER", DefaultLanguageHighlighterColors.FUNCTION_CALL))
+        tokenHighlighters[TableGenTypes.BANGXOR] =
+            arrayOf(createTextAttributesKey("TABLEGEN_BANG_OPERATOR_XOR", DefaultLanguageHighlighterColors.FUNCTION_CALL))
     }
 
     override fun getTokenHighlights(tokenType: IElementType?): Array<TextAttributesKey> {
